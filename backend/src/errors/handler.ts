374b551cb75844fabc668fb29f17adb05685651b
Azanniel <leo.azannielttt@gmail.com>
2020-10-14 08:16:15 -0400

    [backend] 🐛 Resolvido problema do salvamento de imagens
    Quando uma requisição era enviada e gerava um erro a imagem era salva mesmo assim
    agora já é possível perceber que não há mais esse problema

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,5 +1,6 @@
-import { ErrorRequestHandler } from 'express'
+import { ErrorRequestHandler, Express } from 'express'
 import { ValidationError } from 'yup'
+import fs from 'fs'
 
 interface ValidationErrors {
   [key: string]: string[]
@@ -8,6 +9,9 @@ interface ValidationErrors {
 const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
   if (error instanceof ValidationError) {
     const errors: ValidationErrors = {}
+
+    const requestImages = request.files as Express.Multer.File[]
+    requestImages.map(image => fs.unlinkSync(image.path))
 
     error.inner.forEach(err => {
       errors[err.path] = err.errors
